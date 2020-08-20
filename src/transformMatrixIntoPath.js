cae00d3ef48131951c7939278878a574a72c1143
Robert Long <rlong@sfox.com>
2020-08-20 12:56:26 -0400

    Add the square property and fix the math to work for any given size

--- src/transformMatrixIntoPath.js
+++ src/transformMatrixIntoPath.js
@@ -1,20 +1,21 @@
 export default (matrix, size) => {
   const cellSize = size / matrix.length
+  const adjustSize = size * 0.024
   let path = ''
   matrix.forEach((row, i) => {
     let needDraw = false
     row.forEach((column, j) => {
       if (column) {
         if (!needDraw) {
-          path += `M${cellSize * j} ${cellSize / 2 + cellSize * i} `
+          path += `M${cellSize * j + adjustSize} ${cellSize / 2 + cellSize * i} `
           needDraw = true
         }
         if (needDraw && j === matrix.length - 1) {
-          path += `L${cellSize * (j + 1)} ${cellSize / 2 + cellSize * i} `
+          path += `L${cellSize * (j + adjustSize)} ${cellSize / 2 + cellSize * i} `
         }
       } else {
         if (needDraw) {
-          path += `L${cellSize * j} ${cellSize / 2 + cellSize * i} `
+          path += `L${cellSize * j - adjustSize} ${cellSize / 2 + cellSize * i} `
           needDraw = false
         }
       }
