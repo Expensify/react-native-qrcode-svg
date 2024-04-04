ee7c336b4e9ae31e567e7616d2053b1d32ecdba1
war-in <war-in@users.noreply.github.com>
2024-04-04 11:57:40 +0200

    working ios, android and web (but web without QRCode)

--- Example/metro.config.js
+++ Example/metro.config.js
@@ -46,4 +46,6 @@ const config = {
     },
 };
 
+console.log("di[a");
+
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
---
+++ Example/src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+    SafeAreaView,
+    StatusBar,
+    Text,
+    View
+} from 'react-native';
+import QRCode from "react-native-qrcode-svg";
+
+const App = () => {
+    return (
+        <SafeAreaView>
+            <StatusBar barStyle='dark-content' />
+            <View style={{alignItems: 'center'}}>
+                <Text style={{fontSize: 24}}>React Native Web App Example</Text>
+
+                <QRCode
+                    value={"https://www.google.com/"}
+                    size={200}
+                />
+
+            </View>
+        </SafeAreaView>
+    );
+};
+export default App;
