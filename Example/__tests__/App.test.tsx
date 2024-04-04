6adc4313c6055b0c2f407ebe735123ce7519d6db
war-in <war-in@users.noreply.github.com>
2024-04-04 11:57:40 +0200

    working ios, android and web (but web without QRCode)

--- Example/__tests__/App.test.tsx
+++ Example/__tests__/App.test.tsx
@@ -4,7 +4,7 @@
 
 import 'react-native';
 import React from 'react';
-import App from '../App';
+import App from '../src/App.tsx';
 
 // Note: import explicitly to use the types shipped with jest.
 import {it} from '@jest/globals';
